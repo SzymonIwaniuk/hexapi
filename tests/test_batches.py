4f2c93424fa4d560a62ffe10f65e5fe3daeccc47
SzymonIwaniuk <s.f.iwaniuk@gmail.com>
2025-05-19 00:26:58 +0200

    feat: Add more tests for can_allocate

--- tests/test_batches.py
+++ tests/test_batches.py
@@ -2,10 +2,39 @@ from domain.model import Batch, Orderline
 from datetime import date
 
 
+#helper function
+def make_batch_and_line(sku, batch_qty, line_qty):
+    return(
+        Batch("batch-001", sku, batch_qty, eta=date.today()),
+        Orderline('order-123', sku, line_qty),
+    )
+
+
 def test_allocating_to_a_batch_reduces_available_quantity() -> None:
-    batch = Batch("batch-001", "SMALL-TABLE", qty=20, eta=date.today())
-    line = Orderline("order-ref", "SMALL-TABLE", 2)
+    batch = Batch("batch-001", "SONY-HEADPHONES", qty=20, eta=date.today())
+    line = Orderline("order-ref", "SONY-HEADPHONES", 2)
 
     batch.allocate(line)
 
     assert batch.available_quantity == 18
+
+
+def test_can_allocate_if_available_grater_than_required() -> None:
+    large_batch, small_line = make_batch_and_line("JBL-HEADPHONES", 20, 2)
+    assert large_batch.can_allocate(small_line)
+
+
+def test_cannot_allocate_if_available_grater_than_required() -> None:
+    small_batch, large_line = make_batch_and_line("MICROPHONE", 3, 15)
+    assert small_batch.can_allocate(large_line) is False
+
+
+def test_can_allocate_if_available_equal_to_required() -> None:
+    batch, line = make_batch_and_line("SONY-LINKBUDS", 2, 2)
+    assert batch.can_allocate(line)
+
+
+def test_cannot_allocate_if_skus_do_not_match() -> None:
+    batch = Batch("batch-001", "UNCOMFORTABLE-HEADPHONES", qty=1, eta=None)
+    different_sku_line = Orderline("order-123", "EXPENSIVE-IPAD", 100)
+    assert batch.can_allocate(different_sku_line) is False
